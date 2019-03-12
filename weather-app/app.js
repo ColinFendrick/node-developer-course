7c91c92055844b8316b302ab763acd38d6898be6
Colin Fendrick <colin.fendrick@gmail.com>
2019-03-12 07:54:40 -0400

    Add in logging for data points

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/17d2639d3d7edac460b5d2a1c741e916/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/17d2639d3d7edac460b5d2a1c741e916/27.950575,-82.457176';
 
-request({ url }, (error, res) => {
-  const data = JSON.parse(res.body);
-  console.log(data.currently)
+request({ url, json: true }, (error, { body: { currently, daily }}) => {
+  console.log(`It's currently ${Math.round(currently.temperature)}°. There is a ${currently.precipProbability}% change of rain.
+    \nToday it will be ${daily.data[0].summary.toLowerCase()}`)
 });
