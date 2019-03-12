8b4360dea44a8301e062513114263a704daf0c0b
Colin Fendrick <colin.fendrick@gmail.com>
2019-03-12 07:09:26 -0400

    Add npm and make api request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,8 @@
-console.log('start')
+const request = require('request');
 
-setTimeout(() => {
-  
-}, 1200)
+const url = 'https://api.darksky.net/forecast/17d2639d3d7edac460b5d2a1c741e916/37.8267,-122.4233';
 
-console.log('stopping')
+request({ url }, (error, res) => {
+  const data = JSON.parse(res.body);
+  console.log(data.currently)
+});
