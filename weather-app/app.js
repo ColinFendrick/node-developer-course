3a7a4d2400d8a127e2336fd5ad9a671184adb079
Colin Fendrick <colin.fendrick@gmail.com>
2019-03-13 08:58:00 -0400

    Create error handler function and callbackl

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,27 +3,29 @@ const request = require('request');
 const url = 'https://api.darksky.net/forecast/17d2639d3d7edac460b5d2a1c741e916/27.950575,-82.457176';
 const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2ZlbmRyaWNrIiwiYSI6ImNqdDV1ajE2YzA4MWI0M21tdnB2MzVpZHUifQ.WHwlpNOqbRSqhmGfkwgiUQ&limit=1';
 
+const handleResponse = (error, res, callback) => {
+  if (error) { console.log(error); }
+  else if (res.body.error) { console.log(res.body.error); }
+  else { callback(res); }
+};
+
 request({ url, json: true }, (error, res) => {
-  if (error) {
-    console.log('Unable to connect to weather service.');
-  } else if (res.body.error) {
-    console.log(res.body.error);
-  } else {
-    console.log(`It's currently ${Math.round(res.body.currently.temperature)}°. There is a ${res.body.currently.precipProbability}% change of rain.
-      \nToday it will be ${res.body.daily.data[0].summary.toLowerCase()}`);
-  };
+  const success = () => console.log(
+    `It's currently ${Math.round(res.body.currently.temperature)}°. There is a ${res.body.currently.precipProbability}% change of rain.
+    \nToday it will be ${res.body.daily.data[0].summary.toLowerCase()}`
+  );
+
+  handleResponse(error, res, success);
 });
 
 request({ url: geoUrl, json: true }, (error, res) => {
-  if (error) {
-    console.log('Unable to connect to location services.');
-  } else if (res.body.error) {
-    console.log(res.body.error);
-  } else if (res.body.features.length === 0) {
-    console.log('Cannot find any matching locations.')
-  } else {
-    const longitude = res.body.features[0].center[0];
-    const latitude = res.body.features[0].center[1];
-    console.log(longitude, latitude);
-  }
+  const locationResponseHandler = () => {
+    if (res.body.features.length === 0) { console.log('Cannot find any matching locations.') }
+    else {
+      const longitude = res.body.features[0].center[0];
+      const latitude = res.body.features[0].center[1];
+      console.log(longitude, latitude);}
+  };
+
+  handleResponse(error, res, locationResponseHandler);
 });
