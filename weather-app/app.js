48dd6530a06e350292f4d2c0083f60e04d77a037
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-01 15:40:15 -0400

    Update to latest before 7

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,54 +1,23 @@
-const request = require('request');
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
-const { geoCode } = require('./utils/geocode');
+const address = process.argv[2]
 
-// const url = 'https://api.darksky.net/forecast/17d2639d3d7edac460b5d2a1c741e916/27.950575,-82.457176';
-// const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2ZlbmRyaWNrIiwiYSI6ImNqdDV1ajE2YzA4MWI0M21tdnB2MzVpZHUifQ.WHwlpNOqbRSqhmGfkwgiUQ&limit=1';
+if (!address) {
+  console.log('Please provide an address')
+} else {
+  geocode(address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return console.log(error)
+    }
 
-// const handleResponse = (error, res, callback) => {
-//   if (error) { console.log(error); }
-//   else if (res.body.error) { console.log(res.body.error); }
-//   else { callback(res); }
-// };
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error)
+      }
 
-// const geocode = (address, callback) => {
-//   request({ url: geoUrl, json: true }, (error, res) => {
-//     const locationResponseHandler = () => {
-//       if (res.body.features.length === 0) { console.log('Cannot find any matching locations.') }
-//       else {
-//         const longitude = res.body.features[0].center[0];
-//         const latitude = res.body.features[0].center[1];
-//         console.log(longitude, latitude);
-//       }
-//     };
-
-//     handleResponse(error, res, locationResponseHandler);
-//   });
-// }
-
-// request({ url, json: true }, (error, res) => {
-//   const success = () => console.log(
-//     `It's currently ${Math.round(res.body.currently.temperature)}°. There is a ${res.body.currently.precipProbability}% change of rain.
-//     \nToday it will be ${res.body.daily.data[0].summary.toLowerCase()}`
-//   );
-
-//   handleResponse(error, res, success);
-// });
-
-// request({ url: geoUrl, json: true }, (error, res) => {
-//   const locationResponseHandler = () => {
-//     if (res.body.features.length === 0) { console.log('Cannot find any matching locations.') }
-//     else {
-//       const longitude = res.body.features[0].center[0];
-//       const latitude = res.body.features[0].center[1];
-//       console.log(longitude, latitude);}
-//   };
-
-//   handleResponse(error, res, locationResponseHandler);
-// });
-
-
-geoCode('Philadelphia', (error, data) => {
-  console.log(error);
-  console.log(data);
-});
+      console.log(location)
+      console.log(forecastData)
+    })
+  })
+}
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,34 +1,21 @@
-const request = require('request');
+const request = require('request')
 
-const geoCode = (address, callback) => {
-  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiY2ZlbmRyaWNrIiwiYSI6ImNqdDV1ajE2YzA4MWI0M21tdnB2MzVpZHUifQ.WHwlpNOqbRSqhmGfkwgiUQ&limit=1';
-  request({ url, json: true }, (error, res) => {
+const geocode = (address, callback) => {
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
-      callback('Unable to connect to location services');
-    } else if (res.body.features.length === 0) {
-      callback('Unable to find location. Try another search.');
+      callback('Unable to connect to location services!', undefined)
+    } else if (body.features.length === 0) {
+      callback('Unable to find location. Try another search.', undefined)
     } else {
-      callback(error, {
-        latitude: res.body.features[0].center[0],
-        longitude: res.body.features[0].center[1],
-        location: res.body.features[0].place_name
-      });
-    };
-  });
-};
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
+      })
+    }
+  })
+}
 
-const forecast = (lat, lon, callback) => {
-  const url = 'https://api.darksky.net/forecast/17d2639d3d7edac460b5d2a1c741e916/' + lat + ',' + lon;
-  request({ url, json: true}, (error, res) => {
-    if (error) {
-      callback('Cannot connect to forecast services');
-    } else if (res) {
-      console.log(
-        `It's currently ${Math.round(res.body.currently.temperature)}°. There is a ${res.body.currently.precipProbability}% change of rain.
-        \nToday it will be ${res.body.daily.data[0].summary.toLowerCase()}`
-      );
-    };
-  });
-};
-
-module.exports = { geoCode, forecast };
+module.exports = geocode
