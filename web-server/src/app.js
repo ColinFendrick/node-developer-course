7fc9345ac486812c92b8e2af10ff6f2529fe10df
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-03 15:48:27 -0400

    Add html static assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,8 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
-
-app.get('', (req, res) => {
-  res.send('<h1>Weather</h1>')
-})
-
-app.get('/help', (req, res) => {
-  res.send([{
-    name: 'Colin',
-    age: 27
-  }, {
-      name: 'Colin',
-      age: 27
-    }])
-})
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>')
-})
+app.use(express.static(path.join(__dirname, '..', '/public')))
 
 app.get('/weather', (req, res) => {
   res.send({
