62231998527f334d5388070efe6e9fdf6dc8749e
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-03 15:18:00 -0400

    Update serves

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,28 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-  res.send('Hello express!')
+  res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-  res.send('Help page')
+  res.send([{
+    name: 'Colin',
+    age: 27
+  }, {
+      name: 'Colin',
+      age: 27
+    }])
 })
 
 app.get('/about', (req, res) => {
-  res.send('Title Page')
+  res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
-  res.send('Weather')
+  res.send({
+    forecast: 'Sunny',
+    location: 'Tampa'
+  })
 })
 
 app.listen(3000, () => console.log('server is up on port 3000')) 
