848e0d7a3b414b3b2080bc11e22f4a6a134b1037
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-09 12:00:13 -0400

    Use handlebars for dynamic html generation

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,7 +2,30 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
+
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '..', '/public')))
+
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Colin'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Colin'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'Help here'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
