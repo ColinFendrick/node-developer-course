df3fd3e33ca75208dc2872bfaf68e48bf5d7a7c6
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-10 09:32:42 -0400

    Add template partials and views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,19 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '..', '/public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Configure Express and set paths
 app.use(express.static(publicDirectoryPath))
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Define routes
 app.get('', (req, res) => {
@@ -29,7 +32,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    message: 'Help here'
+    message: 'Help Page',
+    title: 'Help Page',
+    name: 'Colin'
   })
 })
 
