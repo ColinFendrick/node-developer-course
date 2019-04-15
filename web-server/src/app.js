7af3f5c59d391ad7ccf0216da1c1c9546a5b30fd
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-15 14:49:09 -0400

    Add req query handling in weather

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -39,9 +39,26 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({ error: 'Address must be provided' })
+  }
+
   res.send({
     forecast: 'Sunny',
     location: 'Tampa'
+  })
+})
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+
+  console.log(req.query)
+  res.send({
+    products: []
   })
 })
 
