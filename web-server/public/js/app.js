f46c4829b48b4405acb8d713cab0f07bdc418a36
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-17 09:00:49 -0400

    Update script to hit our endpoint

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,5 @@
-console.log('loaded the client-side js')
+fetch('http://localhost:3000/weather?address=boston').then(res => {
+  res.json().then(data => {
+    data.error ? console.log(data.error) : console.log(data.forecast)
+  })
+})
