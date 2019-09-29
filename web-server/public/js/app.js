8a02bec60a90308112a7c557f7af7b50fb139b88
Colin Fendrick <colin.fendrick@gmail.com>
2019-09-29 15:21:56 -0400

    Parse special characters

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,11 @@
-console.log('Client side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
+const searchForm = document.querySelector('input')
+const messageOne = document.querySelector()
+const messageTwo = document.querySelector()
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(`http://localhost:3000/weather?address=${search.value}`).then(res => {
+  fetch(`http://localhost:3000/weather?address=${searchForm.value.replace(/[^a-zA-Z ]/g, "")}`).then(res => {
     res.json().then(data => {
       if (data.error) {
         console.log(data.error)
