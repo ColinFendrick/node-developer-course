a6bcd5683de2bc21e6a86319cfb4571392e8d494
Colin Fendrick <colin.fendrick@gmail.com>
2019-09-30 14:07:34 -0400

    Delete dupes

--- playgroud/1-json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const dataBuffer = fs.readFileSync('1-json.json');
-const data = JSON.parse(dataBuffer.toString());
-
-data.name = 'Colin Fendrick';
-data.age = 28;
-fs.writeFileSync('1-json.json', JSON.stringify(data));
-console.log(data);
--- playgroud/defaults.js
+++
@@ -1,6 +0,0 @@
-const desTest = ({ name = 'boi' } = {}) => console.log(name);
-
-desTest(undefined)
-// desTest(null)
-desTest({})
-desTest({ age: '1' })
